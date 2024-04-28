f503d881595911072de24fe7617d2c7274c1ef72
Eduardo Folly <edufolly@gmail.com>
2024-04-28 18:12:57 -0300

    Updating to Quarkus 3.9.5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.allopen") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.allopen") version "1.9.23"
     id("io.quarkus")
 }
 
@@ -55,6 +55,16 @@ tasks.withType<Test> {
         "java.util.logging.manager",
         "org.jboss.logmanager.LogManager"
     )
+
+    testLogging {
+        events(
+            "PASSED",
+            "SKIPPED",
+            "FAILED",
+            "STANDARD_OUT",
+            "STANDARD_ERROR"
+        )
+    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
